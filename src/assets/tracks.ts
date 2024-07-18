c30f45997fd2fba7b990f6d94979bd63671725df
James Easter <james.m.easter@gmail.com>
2024-07-17 22:00:09 -0600

    Add assets directory items.

---
+++ src/assets/tracks.ts
@@ -0,0 +1,41 @@
+// Audio track images
+import photo from "./images/1x1_plant.jpg";
+
+// List of tracks that are passed to audio player
+export const tracks = [
+  {
+    title: "Day Dreamer",
+    artist: "James Easter",
+    audioSrc: "/tracks/Day_Dreamer.m4a",
+    image: photo,
+    color: "white",
+  },
+  {
+    title: "Ritual",
+    artist: "James Easter",
+    audioSrc: "/tracks/Ritual.m4a",
+    image: photo,
+    color: "blue",
+  },
+  {
+    title: "Should I Care",
+    artist: "James Easter",
+    audioSrc: "/tracks/Should_I_Care.m4a",
+    image: photo,
+    color: "red",
+  },
+  {
+    title: "Migration",
+    artist: "James Easter",
+    audioSrc: "/tracks/Migration.m4a",
+    image: photo,
+    color: "purple",
+  },
+  // {
+  //   title: "Self Talk",
+  //   artist: "James Easter",
+  //   audioSrc: "/tracks/Self_Talk.m4a",
+  //   image: photo,
+  //   color: "purple",
+  // },
+];
---
+++ src/assets/videos.ts
@@ -0,0 +1,190 @@
+// PDF Imports
+import Emily from "./pdf/Emily.pdf";
+import Softly from "./pdf/Softly.pdf";
+import Witch_Hunt from "./pdf/Witch_Hunt.pdf";
+import Stablemates from "./pdf/Stablemates.pdf";
+import Ill_Be_Seeing_You from "./pdf/Ill_Be_Seeing_You.pdf";
+import Second_Balcony_Jump from "./pdf/Second_Balcony_Jump.pdf";
+import It_Could_Happen_To_You from "./pdf/It_Could_Happen_To_You.pdf";
+import Peter_Bernstein_Lady_Bird from "./pdf/Peter_Bernstein_Lady_Bird.pdf";
+import Wes_Montgomery_Satin_Doll from "./pdf/Wes_Montgomery_Satin_Doll.pdf";
+import Miles_Davis_Milestones_old from "./pdf/Miles_Davis_Milestones_old.pdf";
+
+// List of transcription videos that are passed to video player
+export const allVideos = [
+  {
+    title: "Transcriptions",
+    info: [
+      {
+        id: "LJc4CPRfFoM",
+        image: "https://img.youtube.com/vi/LJc4CPRfFoM/maxresdefault.jpg",
+        title: "Steve Masakowski",
+        sub_title: "Emily",
+        url: "https://www.youtube.com/watch?v=LJc4CPRfFoM",
+        small_title: true,
+        pdf: Emily,
+      },
+      {
+        id: "eZyFPcJjOkA",
+        image: "https://img.youtube.com/vi/eZyFPcJjOkA/maxresdefault.jpg",
+        title: "Dexter Gordon",
+        sub_title: "Second Balcony Jump",
+        url: "https://www.youtube.com/watch?v=eZyFPcJjOkA",
+        pdf: Second_Balcony_Jump,
+      },
+      {
+        id: "z3FH5_C_pzo",
+        image: "https://img.youtube.com/vi/z3FH5_C_pzo/maxresdefault.jpg",
+        title: "Miles Davis",
+        sub_title: "Milestones (old)",
+        url: "https://www.youtube.com/watch?v=z3FH5_C_pzo",
+        pdf: Miles_Davis_Milestones_old,
+      },
+      {
+        id: "nCrCLeIJzm8",
+        image: "https://img.youtube.com/vi/nCrCLeIJzm8/maxresdefault.jpg",
+        title: "Lee Morgan",
+        sub_title: "Stablemates",
+        url: "https://www.youtube.com/watch?v=nCrCLeIJzm8",
+        pdf: Stablemates,
+      },
+      {
+        id: "Bq5qf50d-cI",
+        image: "https://img.youtube.com/vi/Bq5qf50d-cI/maxresdefault.jpg",
+        title: "Herbie Hancock",
+        sub_title: "Witch Hunt",
+        url: "https://www.youtube.com/watch?v=Bq5qf50d-cI",
+        pdf: Witch_Hunt,
+      },
+      {
+        id: "gIe8VBxFGOM",
+        image: "https://img.youtube.com/vi/gIe8VBxFGOM/maxresdefault.jpg",
+        title: "Kurt Rosenwinkel",
+        sub_title: "I'll Be Seeing You",
+        url: "https://www.youtube.com/watch?v=gIe8VBxFGOM",
+        small_title: true,
+        pdf: Ill_Be_Seeing_You,
+      },
+      {
+        id: "lYYy3sZMRwQ",
+        image: "https://img.youtube.com/vi/lYYy3sZMRwQ/maxresdefault.jpg",
+        title: "Wes Montgomery",
+        sub_title: "Montgomeryland Funk",
+        small_title: true,
+        url: "https://www.youtube.com/watch?v=lYYy3sZMRwQ",
+        // pdf: ,
+      },
+      {
+        id: "lGysbDsvNOU",
+        image: "https://img.youtube.com/vi/lGysbDsvNOU/maxresdefault.jpg",
+        title: "Wes Montgomery",
+        sub_title: "Satin Doll",
+        small_title: true,
+        url: "https://www.youtube.com/watch?v=lGysbDsvNOU",
+        pdf: Wes_Montgomery_Satin_Doll,
+      },
+      {
+        id: "uBfEOiovs9g",
+        image: "https://img.youtube.com/vi/uBfEOiovs9g/maxresdefault.jpg",
+        title: "Bill Hardman",
+        sub_title: "Softly",
+        url: "https://www.youtube.com/watch?v=uBfEOiovs9g",
+        pdf: Softly,
+      },
+      {
+        id: "rW97jsCq9A8",
+        image: "https://img.youtube.com/vi/rW97jsCq9A8/maxresdefault.jpg",
+        title: "Miles Davis",
+        sub_title: "It Could Happen To You",
+        url: "https://www.youtube.com/watch?v=rW97jsCq9A8",
+        pdf: It_Could_Happen_To_You,
+      },
+      {
+        id: "7Dy6AteuOHE",
+        image: "https://img.youtube.com/vi/7Dy6AteuOHE/maxresdefault.jpg",
+        title: "Peter Bernstein",
+        sub_title: "Lady Bird",
+        url: "https://www.youtube.com/watch?v=7Dy6AteuOHE",
+        pdf: Peter_Bernstein_Lady_Bird,
+      },
+    ],
+  },
+
+  {
+    title: "Live",
+    info: [
+      {
+        id: "A-8_s_-AYUM",
+        image: "https://img.youtube.com/vi/A-8_s_-AYUM/maxresdefault.jpg",
+        sub_title: "Girl From Ipanema (Jobim)",
+        title: "@Celebration Pointe",
+        url: "https://www.youtube.com/watch?v=A-8_s_-AYUM",
+        small_title: true,
+      },
+      {
+        id: "oH0bN-_YZd4",
+        image: "https://img.youtube.com/vi/oH0bN-_YZd4/maxresdefault.jpg",
+        sub_title: "It Could Happen To You (Van-Heusen)",
+        title: "@The Hippodrome",
+        url: "https://www.youtube.com/watch?v=oH0bN-_YZd4",
+        small_subtitle: true,
+      },
+      {
+        id: "wTY8S_s-z-k",
+        image: "https://img.youtube.com/vi/wTY8S_s-z-k/maxresdefault.jpg",
+        sub_title: "Antrhopology (Parker)",
+        title: "@The Hippodrome",
+        url: "https://www.youtube.com/watch?v=wTY8S_s-z-k",
+      },
+      {
+        id: "QC0a46cX5Sw",
+        image: "https://img.youtube.com/vi/QC0a46cX5Sw/maxresdefault.jpg",
+        sub_title: "Lady Be Good (Gerswhin)",
+        title: "@Little Gem Saloon",
+        url: "https://www.youtube.com/watch?v=QC0a46cX5Sw",
+        small_title: true,
+      },
+      {
+        id: "Ka9EV91Yku8",
+        image: "https://img.youtube.com/vi/Ka9EV91Yku8/maxresdefault.jpg",
+        sub_title: "Out of Nowhere (Green)",
+        title: "@Apple Barrel",
+        url: "https://www.youtube.com/watch?v=Ka9EV91Yku8",
+      },
+      {
+        id: "kIeHv78cMn8",
+        image: "https://img.youtube.com/vi/kIeHv78cMn8/maxresdefault.jpg",
+        sub_title: "Blues by Five (Garland)",
+        title: "@Old Point Bar",
+        url: "https://www.youtube.com/watch?v=kIeHv78cMn8",
+      },
+    ],
+  },
+
+  {
+    title: "Studio",
+    info: [
+      {
+        id: "mkLl_BpGbeU",
+        image: "https://img.youtube.com/vi/mkLl_BpGbeU/maxresdefault.jpg",
+        title: "Ritual",
+        sub_title: "James Easter",
+        url: "https://www.youtube.com/watch?v=mkLl_BpGbeU",
+      },
+      {
+        id: "t51AQyXH3I4",
+        image: "https://img.youtube.com/vi/t51AQyXH3I4/maxresdefault.jpg",
+        title: "Beatrice",
+        sub_title: "Sam Rivers",
+        url: "https://www.youtube.com/watch?v=t51AQyXH3I4",
+      },
+      {
+        id: "-28qjhW5Wzw",
+        image: "https://img.youtube.com/vi/-28qjhW5Wzw/maxresdefault.jpg",
+        title: "Peace",
+        sub_title: "Horace Silver",
+        url: "https://www.youtube.com/watch?v=-28qjhW5Wzw",
+      },
+    ],
+  },
+];
