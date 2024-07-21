d6500ac5de4b5e4a84d02ef9dddb41bef6aba815
James Easter <james.m.easter@gmail.com>
2024-07-21 00:52:24 -0600

    Add audio player to music page.

--- src/pages/Music.tsx
+++ src/pages/Music.tsx
@@ -1,14 +1,46 @@
 // Imports
+import { useState } from "react";
 import { Box } from "@mui/material";
 import Page from "../components/Page";
+import { tracks } from "../assets/tracks";
+import AudioPlayer from "../components/AudioPlayer";
 
 /**
  * Music Page
  */
 export default function Music() {
+  // Local State
+  const [trackIndex, setTrackIndex] = useState(0);
+  const [isFirstSong, setIsFirstSong] = useState(true);
+
+  // Skip audio track forward or backward
+  const skip = (to: "next" | "prev") => {
+    setIsFirstSong(false);
+    return to === "next"
+      ? trackIndex < tracks.length - 1
+        ? setTrackIndex(trackIndex + 1)
+        : setTrackIndex(0)
+      : trackIndex - 1 >= 0
+      ? setTrackIndex(trackIndex - 1)
+      : setTrackIndex(tracks.length - 1);
+  };
+
+  // Select audio track from playlist
+  const handleAudioSelect = (key: number) => {
+    setTrackIndex(key);
+  };
+
   return (
     <Page>
-      <Box sx={{ mt: 4 }}>Music</Box>
+      <Box sx={{ mt: 6 }}>
+        <AudioPlayer
+          skip={skip}
+          tracks={tracks}
+          trackIndex={trackIndex}
+          isFirstSong={isFirstSong}
+          handleSelect={handleAudioSelect}
+        />
+      </Box>
     </Page>
   );
 }
