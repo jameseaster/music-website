0629232a3aaf9b409c84fe4161a7d336d91ca18e
James Easter <james.m.easter@gmail.com>
2024-11-04 00:43:49 -0700

    Add support button to homepage.

--- src/components/HomeText.tsx
+++ src/components/HomeText.tsx
@@ -1,7 +1,6 @@
 // Imports
 import { Box } from "@mui/material";
 import AnimatedTrail from "./AnimatedTrail";
-import ColorModeButton from "./ColorModeButton";
 import Typography from "@mui/material/Typography";
 
 /**
@@ -9,45 +8,42 @@ import Typography from "@mui/material/Typography";
  */
 export default function HomeText() {
   return (
-    <>
-      <Box
-        sx={{
-          mb: 4,
-          p: { xs: 2, sm: 4 },
-          borderRadius: "4px",
-          border: (theme) => `1px solid ${theme.palette.text.secondary}`,
-        }}
-      >
-        <AnimatedTrail open={true} endHeight={105}>
-          <Typography
-            variant="h1"
-            color="primary"
-            sx={{ letterSpacing: "0.07em" }}
-          >
-            James
-          </Typography>
-          <Typography
-            variant="h1"
-            color="primary"
-            sx={{ letterSpacing: "0.07em" }}
-          >
-            Easter
-          </Typography>
-          <Typography
-            variant="h1"
-            sx={{
-              letterSpacing: "0.07em",
-              color: (theme) =>
-                theme.palette.mode === "light"
-                  ? theme.palette.grey[700]
-                  : "white",
-            }}
-          >
-            Music
-          </Typography>
-        </AnimatedTrail>
-      </Box>
-      <ColorModeButton />
-    </>
+    <Box
+      sx={{
+        mb: 4,
+        p: { xs: 2, sm: 4 },
+        borderRadius: "4px",
+        border: (theme) => `1px solid ${theme.palette.text.secondary}`,
+      }}
+    >
+      <AnimatedTrail open={true} endHeight={105}>
+        <Typography
+          variant="h1"
+          color="primary"
+          sx={{ letterSpacing: "0.07em" }}
+        >
+          James
+        </Typography>
+        <Typography
+          variant="h1"
+          color="primary"
+          sx={{ letterSpacing: "0.07em" }}
+        >
+          Easter
+        </Typography>
+        <Typography
+          variant="h1"
+          sx={{
+            letterSpacing: "0.07em",
+            color: (theme) =>
+              theme.palette.mode === "light"
+                ? theme.palette.grey[700]
+                : "white",
+          }}
+        >
+          Music
+        </Typography>
+      </AnimatedTrail>
+    </Box>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,42 @@
 // Imports
 import Page from "../components/Page";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 import HomeText from "../components/HomeText";
+import MoneyIcon from "@mui/icons-material/Paid";
+import ColorModeButton from "../components/ColorModeButton";
 
 /**
  * Home Page
  */
 export default function Home() {
+  // Navigate to stripe page
+  const handleSupport = () => {
+    console.log("go to stripe");
+  };
+
   return (
     <Page>
       <HomeText />
+      <Stack
+        direction="row"
+        spacing={2}
+        sx={{
+          width: "280px",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Button
+          variant="contained"
+          onClick={handleSupport}
+          startIcon={<MoneyIcon />}
+          sx={{ textTransform: "none" }}
+        >
+          Support
+        </Button>
+        <ColorModeButton />
+      </Stack>
     </Page>
   );
 }
